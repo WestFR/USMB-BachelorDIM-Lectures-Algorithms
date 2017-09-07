c25e4f02f89d7174bfcd80255978af799ba6b557
= <=>
2017-09-07 12:05:37 +0200

    Add functions. Need commentary
    Add max values functions . Need commentarry

--- assignments/Session1/S2_algomaxvalues.py
+++ assignments/Session1/S2_algomaxvalues.py
@@ -4,7 +4,7 @@
 # @brief : A set of generic functions for data management
 ##
 
-def average_above_zero(input_list):
+def max_values(input_list):
 
 	# Variables
 	positive_values_sum = 0
@@ -33,28 +33,3 @@ result = average_above_zero(input_list);
 # Prepare and display message
 message = "The average of positive element of {list_value} is {res} ".format(list_value = input_list , res = result);
 print(message);
-
-
-
-def max_values(input_list):
-
-	if len(input_list) == 0 :
-		raise ValueError("Provided list is empty.");
-
-	max_val = input_list[0];
-
-	# Average of positive elements of a list
-	for item in input_list :
-		# Select only positive items
-		if max_val < item :
-			max_val = item
-
-	return max_val;
-
-myList = [1,3,7];
-
-myMax = max_values(myList);
-message = "My maximum value is : {max}".format(max = myMax)
-print(message)
-
-
