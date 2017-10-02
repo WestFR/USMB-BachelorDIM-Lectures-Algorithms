d8e72e6f430b6f2e90acc25f42370ff85570c4ea
= <=>
2017-10-02 11:49:28 +0200

    Add second & third test

--- assignments/Session1/S2_test.py
+++ assignments/Session1/S2_test.py
@@ -4,11 +4,35 @@
 ##
 
 import pytest
-import S1_algotools
+import S1_algotools as algo
 
-# AVERAGING :
+# Input List
+input_list = [1,2,3,4,-7]
+input_list1 = [2,4,8,10,12]
+
+# AVERAGING
 # Basic testing average function
 def test_average_above_zero () :
-	assert S1_algotools.average_above_zero([1,2,3,4,-7]) == 2.5
+	assert algo.average_above_zero(input_list) == 2.5
+	assert algo.average_above_zero(input_list1) == 7.2
 
-test_average_above_zero()
+# TABLE MAXIMUM VALUE
+# Basic max_value testing function
+def test_max_value () :
+	assert algo.max_value(input_list) == 4
+	assert algo.max_value(input_list1) == 12
+
+# TABLE MINIMUM VALUE
+# Basic min_value testing function
+def test_min_value () :
+	assert algo.min_value(input_list) == -7
+	assert algo.min_value(input_list1) == 2
+
+
+# REVERSE TABLE
+# Basic reverse_table testing function
+def test_reverse_table () :
+	assert algo.reverse_table(input_list) == [-7,4,3,2,1]
+
+
+
