db3edb37a0cb447df564040d1fb399ca63cca59e
WestFR <steven.francony@gmail.com>
2017-10-24 11:56:35 +0200

    Add msppack - But doesn't work

--- assignments/Session 4/rpc_client.py
+++ assignments/Session 4/rpc_client.py
@@ -7,9 +7,9 @@ Created on Tue Oct 24 09:22:58 2017
 import pika
 import os
 import uuid
-#import msgpack
-#import msgpack_numpy as m
-#import numpy as np
+import msgpack
+import msgpack_numpy as m
+import numpy as np #if Numpy is required.
 
 # Connection paramaters
 amqp_url='amqp://itgfanyr:a63KOZk_gxLVV9TX2Gg9VG4ei5iHslX6@puma.rmq.cloudamqp.com/itgfanyr'
@@ -19,10 +19,11 @@ params.socket_timeout = 5
 
 # Init variables
 callback_queue='rpc_queue'
-message="Hi, how fine ?"
+message={"type":0, "value":"hi, how fine?"}
+encoded_message = msgpack.packb(message, default = m.encode)
 
 class RpcClient(object):
-    def __init__(self):
+    def __init__(self):        
         self.connection = pika.BlockingConnection(params) 
         self.channel = self.connection.channel()
 
@@ -35,6 +36,8 @@ class RpcClient(object):
     def on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
             self.response = body
+            decoded_response = msgpack.unpackb(str(self.response), object_hook = m.decode)
+            print(decoded_response['value'])
 
     def call(self, n):
         self.response = None
@@ -45,15 +48,16 @@ class RpcClient(object):
                                          reply_to = self.callback_queue,
                                          correlation_id = self.corr_id,
                                          ),
-                                   body=str(message))
+                                   body=encoded_message)
+                                   #message
+                                   
         while self.response is None:
             self.connection.process_data_events()
         return str(self.response)
 
 rpc = RpcClient()
 
-
-response = rpc.call(message)
+response = rpc.call(encoded_message)
 
 print(" [x] Requesting")
 print(" [.] Server :" + response)
--- assignments/Session 4/rpc_server.py
+++ assignments/Session 4/rpc_server.py
@@ -8,9 +8,9 @@ Created on Tue Oct 24 09:17:15 2017
 import pika
 import os
 import uuid
-#import msgpack
-#import msgpack_numpy as m
-#import numpy as np
+import msgpack
+import msgpack_numpy as m
+import numpy as np
 
 # Connection paramaters
 amqp_url='amqp://itgfanyr:a63KOZk_gxLVV9TX2Gg9VG4ei5iHslX6@puma.rmq.cloudamqp.com/itgfanyr'
@@ -25,13 +25,21 @@ channel.queue_declare(queue='rpc_queue')
 
 # Function to process the message sent and reply to
 def on_request(ch, method, props, body):
-    print(" [.] Client : " + str(body))
+    
+    decoded_message = msgpack.unpackb(str(body), object_hook = m.decode)
+    print("Message received from client : " + decoded_message['value'])
+
+    #response = "I'm fine"
+    response = {"type":0, "value":"I'm fine !"}
+    
+    encoded_message = msgpack.packb(response, default = m.encode)
+    print("Message sent to the client : " + response['value'])
     
     ch.basic_publish(exchange='',
                      routing_key=props.reply_to,
                      properties=pika.BasicProperties(correlation_id = \
                                                          props.correlation_id),
-                     body=str("I'm Fine"))
+                     body=encoded_message)
                     
     # Acknowledge
     ch.basic_ack(delivery_tag = method.delivery_tag)
