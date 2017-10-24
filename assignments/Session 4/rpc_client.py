65b64ebac661a4fe3229f88a34a0581399207439
WestFR <steven.francony@gmail.com>
2017-10-24 11:03:55 +0200

    Add rpc_client & rpc_server remote procedure

---
+++ assignments/Session 4/rpc_client.py
@@ -0,0 +1,59 @@
+"""
+Created on Tue Oct 24 09:22:58 2017
+@author: Steven Francony
+@brief : Simple example of a remote procedure (client side)
+"""
+
+import pika
+import os
+import uuid
+#import msgpack
+#import msgpack_numpy as m
+#import numpy as np
+
+# Connection paramaters
+amqp_url='amqp://itgfanyr:a63KOZk_gxLVV9TX2Gg9VG4ei5iHslX6@puma.rmq.cloudamqp.com/itgfanyr'
+url = os.environ.get('CLOUDAMQP_URL',amqp_url)
+params = pika.URLParameters(url)
+params.socket_timeout = 5
+
+# Init variables
+callback_queue='rpc_queue'
+message="Hi, how fine ?"
+
+class RpcClient(object):
+    def __init__(self):
+        self.connection = pika.BlockingConnection(params) 
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(self.on_response, no_ack=True,
+                                   queue=self.callback_queue)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, n):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(exchange='',
+                                   routing_key='rpc_queue',
+                                   properties=pika.BasicProperties(
+                                         reply_to = self.callback_queue,
+                                         correlation_id = self.corr_id,
+                                         ),
+                                   body=str(message))
+        while self.response is None:
+            self.connection.process_data_events()
+        return str(self.response)
+
+rpc = RpcClient()
+
+
+response = rpc.call(message)
+
+print(" [x] Requesting")
+print(" [.] Server :" + response)
---
+++ assignments/Session 4/rpc_server.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 09:17:15 2017
+@author: Steven Francony
+@brief : Simple example of a remote procedure (server side)
+"""
+
+import pika
+import os
+import uuid
+#import msgpack
+#import msgpack_numpy as m
+#import numpy as np
+
+# Connection paramaters
+amqp_url='amqp://itgfanyr:a63KOZk_gxLVV9TX2Gg9VG4ei5iHslX6@puma.rmq.cloudamqp.com/itgfanyr'
+url = os.environ.get('CLOUDAMQP_URL',amqp_url)
+params = pika.URLParameters(url)
+params.socket_timeout = 5
+
+# Init communication
+connection = pika.BlockingConnection(params) 
+channel = connection.channel()
+channel.queue_declare(queue='rpc_queue')
+
+# Function to process the message sent and reply to
+def on_request(ch, method, props, body):
+    print(" [.] Client : " + str(body))
+    
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         props.correlation_id),
+                     body=str("I'm Fine"))
+                    
+    # Acknowledge
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
+# Wait for request
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(on_request, queue='rpc_queue')
+
+# Display messages
+print(" [X] Waiting or messages. To exit press CTRL+C'")
+print(" [x] Awaiting RPC requests")
+channel.start_consuming()
